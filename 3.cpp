ad6006840033728fe1c4b1f453d92518000b1644
DespiteDeath <Kogut_nikita@mail.ru>
2017-11-20 13:53:56 +0300

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+int sumN(int);
+void find(int);
+
+int main(int argc, char const *argv[]) {
+  int N = 5;
+  int sum = sumN(N);
+  find(sum);
+
+  return 0;
+}
+
+int sumN(int x) {
+  int sum = 0;
+  for(int i = 1; i < (x + 1); i++)
+    sum += i;
+
+  return sum;
+}
+
+void find(int sumN) {
+  std::vector<int> v{4, 5, 3};
+  std::vector<int> vF;
+  std::vector<int>::iterator iter_v;
+  std::vector<int>::iterator iter_vF;
+  int _sum = 0;
+
+  for(iter_v = v.begin(); iter_v != v.end(); iter_v++) {
+    std::cout << (*iter_v) << ' ';
+    _sum += (*iter_v);
+  }
+  std::cout << std::endl;
+
+  int sumDelVals = sumN - _sum;
+  int delValFst;
+  int delValSec;
+
+  for(int i = (sumDelVals/2); i != 0; i--)
+    vF.push_back(i);
+
+
+  for(iter_vF = vF.begin(); iter_vF != vF.end(); iter_vF++) {
+    for(iter_v = v.begin(); iter_v != v.end(); iter_v++) {
+      if( (*iter_vF) == (*iter_v) ) {
+        vF.erase( iter_vF );
+      } else {
+        delValFst = (*iter_vF);
+      }
+    }
+  }
+
+  delValSec = sumDelVals - delValFst;
+
+  std::cout << "Deleted : " << delValFst << " \'n\' " << delValSec << std::endl;
+
+  v.push_back(delValFst);
+  v.push_back(delValSec);
+
+  for(iter_v = v.begin(); iter_v != v.end(); iter_v++)
+    std::cout << (*iter_v) << ' ';
+
+  std::cout << std::endl;
+}
